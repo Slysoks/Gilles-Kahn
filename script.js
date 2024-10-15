b30d90910890dedfa9c1a06ce38d426256e74178
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-10-15 23:10:10 +0200

    Refactor code to fetch and render data from info.json

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-const fetchData = async () => {
-  const response = await fetch('./info.json');
-  const data = await response.json();
-  return data;
-};
+const timeline = document.querySelector('.timeline');
 
-const renderData = async (data) => {
-  const main = document.querySelector('main');
-  data.forEach((item) => {
-    const section = document.createElement('section');
-    section.innerHTML = `
-      <div class="imageWrapper">
-        <img src="${item.image}" alt="${item.name}" class="sideImage" />
-      </div>
-      <div class="description">
+const data = fetch('./info.json')
+  .then(response => response.json())
+  .then(data => (
+    data.forEach(item => {
+      const div = document.createElement('div');
+      div.classList.add('checkpoint');
+      div.innerHTML = `
         <div>
-          <h2>
-            ${item.title}
-          </h2>
-          <p>
-            ${item.content}
-          </p>
+          <h2>${item.title}</h2>
+          <p>${item.content}</p>
         </div>
-      </div>
-    `;
-    main.appendChild(section);
-  });
-}
-
-const main = async () => {
-  const data = await fetchData();
-  console.log(data);
-  renderData(data);
-};
-
-main();
+      `;
+      timeline.appendChild(div);
+    })
+  ));
